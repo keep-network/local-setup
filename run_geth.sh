59a180028973fabd4c161f9ce1a669e0db752498
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-05-19 13:40:30 +0200

    Geth script

---
+++ run_geth.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+LOG_START='\n\e[1;36m' # new line + bold + color
+LOG_END='\n\e[0m' # new line + reset color
+
+ETHEREUM_DIR="$PWD/ethereum"
+GETH_DATA_DIR="$ETHEREUM_DIR/data"
+GETH_ETHEREUM_ACCOUNT="0x3d373d872b7ba29d92ed47caa8605b4dd6ec84ef"
+
+printf "${LOG_START}Removing old chain data...${LOG_END}"
+
+rm -rf "$GETH_DATA_DIR/geth"
+
+printf "${LOG_START}Initializing geth...${LOG_END}"
+
+geth --datadir=$GETH_DATA_DIR init $ETHEREUM_DIR/genesis/genesis.json
+
+printf "${LOG_START}Starting geth...${LOG_END}"
+
+geth --port 3000 --networkid 1101 --identity "somerandomidentity" \
+    --ws --wsaddr "127.0.0.1" --wsport "8546" --wsorigins "*" \
+    --rpc --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain "*" \
+    --rpcapi "db,ssh,miner,admin,eth,net,web3,personal,debug" \
+    --wsapi "db,ssh,miner,admin,eth,net,web3,personal" \
+    --datadir=$GETH_DATA_DIR --syncmode "fast" \
+    --miner.etherbase=$GETH_ETHEREUM_ACCOUNT --mine --miner.threads=1 \
+    --allow-insecure-unlock
