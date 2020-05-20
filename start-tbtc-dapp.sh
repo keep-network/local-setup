808e3691dcd8b66df0c19ea19fe0ce7863b7fd4c
Jakub Nowakowski <jakub.nowakowski@akena.co>
2020-05-20 16:05:47 +0200

    Added script to start tbtc-dapp
    The script starts local instance of tbtc-dapp.

---
+++ start-tbtc-dapp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+LOG_START='\n\e[1;36m' # new line + bold + color
+LOG_END='\n\e[0m' # new line + reset color
+DONE_START='\n\e[1;32m' # new line + bold + green
+DONE_END='\n\n\e[0m'    # new line + reset
+
+WORKDIR=$PWD
+
+printf "${LOG_START}Starting tBTC dApp...${LOG_END}"
+
+cd $WORKDIR/tbtc-dapp
+
+npm run start
