c9fbf9adb8f90e86c5618814a2083bde1f8e9d17
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-05-29 14:53:20 +0200

    Separate relay install and run scripts

--- run-testnet-relay.sh
+++ run-testnet-relay.sh
@@ -9,40 +9,9 @@ DONE_END='\n\n\e[0m'    # new line + reset
 
 WORKDIR=$PWD
 
-printf "${LOG_START}Initializing relay...${LOG_END}"
-
-cd tbtc/solidity
-
-# Get network ID.
-NETWORK_ID_OUTPUT=$(truffle exec ./scripts/get-network-id.js)
-NETWORK_ID=$(echo "$NETWORK_ID_OUTPUT" | tail -1)
-
-# Extract TestnetRelay contract address.
-JSON_QUERY=".networks.\"${NETWORK_ID}\".address"
-TESTNET_RELAY_CONTRACT="$WORKDIR/tbtc/solidity/build/contracts/TestnetRelay.json"
-TESTNET_RELAY_CONTRACT_ADDRESS=$(cat ${TESTNET_RELAY_CONTRACT} | jq "${JSON_QUERY}" | tr -d '"')
-
-printf "${LOG_START}TestnetRelay contract address is: ${TESTNET_RELAY_CONTRACT_ADDRESS}${LOG_END}"
-
-cd $WORKDIR
-
-# Copy all config files to the right relay directory.
-cp -R configs/relays/. relays/maintainer/maintainer/config/
-
-cd relays/maintainer/maintainer/config
-
-# Fill SUMMA_RELAY_CONTRACT env in config file with their actual value.
-sed -i .OLD 's:RELAYCONTRACT:'$TESTNET_RELAY_CONTRACT_ADDRESS':' .my_env_file.env
-rm .my_env_file.env.OLD
-
-cd ../..
-
-pyenv install 3.7.0
-pyenv global 3.7.0
-
-printf "${DONE_START}relay initialized successfully!${DONE_END}"
-
 printf "${LOG_START}Starting relay...${LOG_END}"
+
+cd "$WORKDIR/relays/maintainer/"
 
 # Install python virtualenv.
 pipenv install --python=$(pyenv which python3.7)
