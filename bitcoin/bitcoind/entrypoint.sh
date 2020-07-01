866ea76d603e768146fc5283ca6f117e3bcb0b04
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-07-01 20:07:08 +0200

    Dockerized private bitcoind

---
+++ bitcoin/bitcoind/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+
+mkdir -p datadir
+
+# Start bitcoind.
+./bitcoind -regtest -datadir=datadir \
+  -port=18333 \
+  -rpcport=18332 \
+  -rpcuser=x \
+  -rpcpassword=user123 \
+  -rpcbind=0.0.0.0 \
+  -rpcallowip=0.0.0.0/0 \
+  --daemon
+
+sleep 5
+
+alias btccli='./bitcoin-cli -regtest -datadir=datadir -rpcport=18332 -rpcuser=x -rpcpassword=user123'
+
+# Get an address.
+address=$(btccli getnewaddress)
+
+# Mine some initial blocks to unlock coinbase.
+btccli generatetoaddress 1000 "$address"
+
+trap "exit" INT
+
+# Run mining loop.
+while true
+do
+    hash=$(btccli generatetoaddress 1 "$address" | tr -d '[" \n"]')
+    count=$(btccli getblockcount)
+    printf "Generated block number $count ($hash) using miner address $address\n"
+    sleep 5
+done
+
