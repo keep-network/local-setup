308435edea89dc05ceea4279441c7d2e41a635cc
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-07-13 20:25:08 +0200

    Invoke bash with -l option

--- deployments/packer/provisioning/install-nodejs.sh
+++ deployments/packer/provisioning/install-nodejs.sh
@@ -6,8 +6,9 @@ echo "Installing Node.js and NPM..."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.profile
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.profile
+source ~/.profile
 
 nvm install 14.3.0
 nvm install 11.15.0
