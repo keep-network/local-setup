ed1c00cbc68b411c5b5b300cba68726b6538b01a
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-07-23 10:26:56 +0200

    Add checks to install-commons.sh

--- deployments/local-setup-environment/provisioning/install-commons.sh
+++ deployments/local-setup-environment/provisioning/install-commons.sh
@@ -15,7 +15,12 @@ sudo apt-get install -y \
   python \
   build-essential
 
+if ! [ -x "$(command -v curl)" ]; then echo "curl installation failed"; exit 1; fi
+if ! [ -x "$(command -v wget)" ]; then echo "wget installation failed"; exit 1; fi
+if ! [ -x "$(command -v git)" ]; then echo "git installation failed"; exit 1; fi
 if ! [ -x "$(command -v unzip)" ]; then echo "unzip installation failed"; exit 1; fi
 if ! [ -x "$(command -v jq)" ]; then echo "jq installation failed"; exit 1; fi
+if ! [ -x "$(command -v python)" ]; then echo "python installation failed"; exit 1; fi
+if ! [ -x "$(command -v make)" ]; then echo "build-essential installation failed"; exit 1; fi
 
 echo "Common tools have been installed successfully!"
