db8413bdcff9cb8ede650f9512ecf5a85eb6fa22
Dmitry <dymitr.paremski@keep.network>
2020-07-27 19:11:20 +0200

    Drafting test summary for tbtc deposit / redemption

---
+++ e2e/e2e-summary.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node --experimental-modules
+
+import Web3 from "web3"
+import ProviderEngine from "web3-provider-engine"
+import WebsocketSubprovider from "web3-provider-engine/subproviders/websocket.js"
+import TBTC from "@keep-network/tbtc.js"
+import Subproviders from "@0x/subproviders"
+import BitcoinRpc from "bitcoind-rpc"
+import Bluebird from "bluebird"
+import config from "../configs/bitcoin/config.json"
+
+const bitcoinRpc = new BitcoinRpc(config)
+Bluebird.promisifyAll(bitcoinRpc)
+
+const blocksTimespan = 500
+
+const engine = new ProviderEngine({ pollingInterval: 1000 })
+
+engine.addProvider(
+    // Private key of address 0xd6b0a1ca8f0641b97efec0f1ed73d72e58b38fa5
+    // which corresponds to the account[5].
+    new Subproviders.PrivateKeyWalletSubprovider(
+        "f95e1da038f1fd240cb0c966d8826fb5c0369407f76f34736a5c381da7ca0ecd"
+    )
+)
+engine.addProvider(
+    // Local geth websocket endpoint.
+    new WebsocketSubprovider(
+        {rpcUrl: "ws://127.0.0.1:8546"}
+    )
+)
+
+const web3 = new Web3(engine)
+
+engine.start()
+
+async function run() {
+    const tbtc = await TBTC.withConfig({
+        web3: web3,
+        bitcoinNetwork: "regtest",
+        electrum: {
+            testnetWS: {
+                server: "127.0.0.1",
+                port: 50003,
+                protocol: "ws"
+            }   
+        }
+    })
+
+    const currentBlockNumber = await web3.eth.getBlockNumber()
+    const fromBlock = 0
+    // const fromBlock = currentBlockNumber - blocksTimespan
+ 
+    const createdDepositEvents = await tbtc.Deposit.systemContract.getPastEvents("Created", {fromBlock: fromBlock, toBlock: "latest"})
+
+    const depositAddresses = createdDepositEvents.map(event => event.returnValues._depositContractAddress)
+
+    for (const depositAddress of depositAddresses) {
+        const deposit = await tbtc.Deposit.withAddress(depositAddress)
+
+        const satoshiLotSize = (await deposit.getSatoshiLotSize()).toString()
+        const bitcoinAddress = await deposit.getBitcoinAddress()
+        const currentState = await deposit.getCurrentState()
+
+        console.log("satoshi lot size: ", satoshiLotSize)
+        console.log("bitcoin address: ", bitcoinAddress)
+        console.log("current state: ", currentState)
+    }
+}
+
+
+run()
+    .then(() => {
+        console.log("Test summary completed successfully")
+
+        process.exit(0)
+    })
+    .catch(error => {
+        console.error("Test summary errored out with error: ", error)
+
+        process.exit(1)
+    })
