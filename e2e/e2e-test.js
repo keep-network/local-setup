b23841e16483a72d5df5e68df8db79a86db01628
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-07-29 14:45:45 +0200

    Change options related with ethereum node

--- e2e/e2e-test.js
+++ e2e/e2e-test.js
@@ -17,8 +17,7 @@ program
     .option('--bitcoin-electrum-port <port>', "electrum server port", (port) => parseInt(port, 10), 50003)
     .option('--bitcoin-network <network>', "type of the bitcoin network (\"regtest\"|\"testnet\")", "regtest")
     .option('--bitcoin-depositor-pk <privateKey>', "private key of the Bitcoin depositor in WIF format", "cTj6Z9fxMr4pzfpUhiN8KssVzZjgQz9zFCfh87UrH8ZLjh3hGZKF")
-    .option('--ethereum-host <host>', "ethereum node host", "127.0.0.1")
-    .option('--ethereum-port <port>', "ethereum node port", (port) => parseInt(port, 10), 8546)
+    .option('--ethereum-node <url>', "ethereum node url", "ws://127.0.0.1:8546")
     .option('--ethereum-pk <privateKey>', "private key of ethereum account", "f95e1da038f1fd240cb0c966d8826fb5c0369407f76f34736a5c381da7ca0ecd")
     .parse(process.argv)
 
@@ -40,9 +39,7 @@ engine.addProvider(
     new Subproviders.PrivateKeyWalletSubprovider(program.ethereumPk)
 )
 engine.addProvider(
-    new WebsocketSubprovider({
-        rpcUrl: `ws://${program.ethereumHost}:${program.ethereumPort}`
-    })
+    new WebsocketSubprovider({rpcUrl: program.ethereumNode})
 )
 
 const web3 = new Web3(engine)
