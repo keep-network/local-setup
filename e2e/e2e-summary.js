bd4696016b9d5f4178a622f7d5ad6465d6514fe4
Dmitry <dymitr.paremski@keep.network>
2020-08-04 14:24:25 +0200

    Connecting to eth and bitcoin testnet

--- e2e/e2e-summary.js
+++ e2e/e2e-summary.js
@@ -5,10 +5,18 @@ import ProviderEngine from "web3-provider-engine"
 import WebsocketSubprovider from "web3-provider-engine/subproviders/websocket.js"
 import TBTC from "@keep-network/tbtc.js"
 import Subproviders from "@0x/subproviders"
-import BitcoinRpc from "bitcoind-rpc"
-import Bluebird from "bluebird"
-import config from "../configs/bitcoin/config.json"
-import * as fs from 'fs' 
+import {getTBTCTokenBalance} from "./common.js";
+import program from "commander"
+
+program 
+    .option('--bitcoin-electrum-host <host>')
+    .option('--bitcoin-electrum-port <port>', "electrum server port", (port) => parseInt(port, 10), 50003)
+    .option('--bitcoin-network <network>')
+    .option('--ethereum-node <url>')
+    .option('--ethereum-pk <privateKey>')
+    .parse(process.argv)
+
+console.log("\nScript options values: ", program.opts(), "\n")
 
 const states = {
     '0': 'START',
@@ -25,26 +33,15 @@ const states = {
     '11': 'LIQUIDATED',
   }
   
-const blocksTimespan = 500
-
-const bitcoinRpc = new BitcoinRpc(config)
-Bluebird.promisifyAll(bitcoinRpc)
-
+const blocksTimespan = 10000
 
 const engine = new ProviderEngine({ pollingInterval: 1000 })
 
 engine.addProvider(
-    // Private key of address 0xd6b0a1ca8f0641b97efec0f1ed73d72e58b38fa5
-    // which corresponds to the account[5].
-    new Subproviders.PrivateKeyWalletSubprovider(
-        "f95e1da038f1fd240cb0c966d8826fb5c0369407f76f34736a5c381da7ca0ecd"
-    )
+    new Subproviders.PrivateKeyWalletSubprovider(program.ethereumPk)
 )
 engine.addProvider(
-    // Local geth websocket endpoint.
-    new WebsocketSubprovider(
-        {rpcUrl: "ws://127.0.0.1:8546"}
-    )
+    new WebsocketSubprovider({rpcUrl: program.ethereumNode})
 )
 
 const web3 = new Web3(engine)
@@ -52,9 +49,31 @@ const web3 = new Web3(engine)
 engine.start()
 
 async function run() {
-
     // Set first account as the default account.
     web3.eth.defaultAccount = (await web3.eth.getAccounts())[0]
+    
+    const tbtc = await TBTC.withConfig({
+        web3: web3,
+        bitcoinNetwork: program.bitcoinNetwork,
+        electrum: {
+            testnetWS: {
+                server: program.bitcoinElectrumHost,
+                port: program.bitcoinElectrumPort,
+                protocol: "ws"
+            }
+        }
+    })
+
+    const initialTbtcAccountBalance = await getTBTCTokenBalance(
+        web3,
+        tbtc,
+        web3.eth.defaultAccount
+    )
+
+    console.log(
+        `Initial TBTC balance for account ${web3.eth.defaultAccount} ` +
+        `is: ${initialTbtcAccountBalance}`
+    )
 
     let htmlContent = 
 `
@@ -75,17 +94,6 @@ async function run() {
     </thead>
     <tbody>
 `
-    const tbtc = await TBTC.withConfig({
-        web3: web3,
-        bitcoinNetwork: "regtest",   
-        electrum: {
-            testnetWS: {
-                server: "127.0.0.1",
-                port: 50003,
-                protocol: "ws"
-            }   
-        }
-    })
 
     const currentBlockNumber = await web3.eth.getBlockNumber()
     // const fromBlock = 0
@@ -94,17 +102,16 @@ async function run() {
     const createdDepositEvents = await tbtc.Deposit.systemContract.getPastEvents("Created", {fromBlock: fromBlock, toBlock: "latest"})
 
     const depositAddresses = createdDepositEvents.map(event => event.returnValues._depositContractAddress)
-    
+
     for (const depositAddress of depositAddresses) {
         const deposit = await tbtc.Deposit.withAddress(depositAddress)
-        
+
         const bitcoinAddress = await deposit.getBitcoinAddress()
         const satoshiLotSize = (await deposit.getSatoshiLotSize()).toString()
         const currentState = await deposit.getCurrentState()
         const signerFee = await deposit.getSignerFeeTBTC()
         const redemptionCost = await deposit.getRedemptionCost()
         const tbtcAccountBalance = await tbtc.Deposit.tokenContract.methods.balanceOf(depositAddress).call()
-
 
         htmlContent += 
         `
