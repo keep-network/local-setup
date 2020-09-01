eb1a00a1c5fb875eb164528fd6b6b1bef21a0a49
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-09-01 13:46:55 +0200

    Refactor config files and add keep-core generator

--- deployments/local-setup-kubernetes/generate.go
+++ deployments/local-setup-kubernetes/generate.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/crypto"
+	libp2pcrypto "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
 	"io/ioutil"
 	"os"
 	"strconv"
@@ -39,6 +42,7 @@ func main() {
 		PrivateKey string
 	}
 
+	var bootstrapID string
 	keyfiles := make([]*keyfile, count)
 
 	for i := 0; i < count; i++ {
@@ -66,6 +70,14 @@ func main() {
 			PrivateKey: hex.EncodeToString(crypto.FromECDSA(key.PrivateKey)),
 		}
 
+		if i == 0 {
+			bootstrapID, err = getIDFromKey(key)
+			if err != nil {
+				fmt.Printf("could get bootstrap id: [%v]\n", err)
+				return
+			}
+		}
+
 		fmt.Printf("generated account %v\n", keyfiles[i].Address)
 	}
 
@@ -74,14 +86,14 @@ func main() {
 	fmt.Printf("generating geth configmap...\n")
 
 	err = generateConfig(
-		"geth-configmap.yml",
+		"network/geth-configmap.yml",
+		"network/geth-configmap.yml",
 		map[string]interface{}{
 			"etherbase": keyfiles[0].Address,
 			"extradata": strings.ToLower(keyfiles[0].Address[2:]),
 			"keyfiles":  keyfiles,
 		},
 	)
-
 	if err != nil {
 		fmt.Printf("could not generate geth configmap: [%v]\n", err)
 		return
@@ -92,27 +104,70 @@ func main() {
 	fmt.Printf("generating accounts configmap...\n")
 
 	err = generateConfig(
-		"accounts-configmap.yml",
+		"network/accounts-configmap.yml",
+		"network/accounts-configmap.yml",
 		map[string]interface{}{
 			"keyfiles": keyfiles,
 		},
 	)
-
 	if err != nil {
 		fmt.Printf("could not generate accounts configmap: [%v]\n", err)
 		return
 	}
 
 	fmt.Printf("accounts configmap generated\n")
+
+	fmt.Printf("generating keep clients configs...\n")
+
+	for i := range keyfiles {
+		err = generateConfig(
+			"clients/keep-client-service.yml",
+			fmt.Sprintf("clients/keep-client-%v-service.yml", i),
+			map[string]interface{}{
+				"clientIndex": i,
+			},
+		)
+		if err != nil {
+			fmt.Printf("could not generate keep-client service config: [%v]\n", err)
+			return
+		}
+
+		err = generateConfig(
+			"clients/keep-client-statefulset.yml",
+			fmt.Sprintf("clients/keep-client-%v-statefulset.yml", i),
+			map[string]interface{}{
+				"clientIndex": i,
+				"bootstrapID": bootstrapID,
+				"owner":       keyfiles[0],
+			},
+		)
+		if err != nil {
+			fmt.Printf("could not generate keep-client statefulset config: [%v]\n", err)
+			return
+		}
+	}
+
+	fmt.Printf("keep clients configs generated\n")
+
 }
 
-func generateConfig(name string, data interface{}) error {
-	tmpl, err := template.ParseFiles("./templates/" + name)
+func getIDFromKey(key *keystore.Key) (string, error) {
+	pk, _ := btcec.PrivKeyFromBytes(btcec.S256(), key.PrivateKey.D.Bytes())
+	id, err := peer.IDFromPrivateKey((*libp2pcrypto.Secp256k1PrivateKey)(pk))
+	if err != nil {
+		return "", err
+	}
+
+	return id.String(), nil
+}
+
+func generateConfig(input, output string, data interface{}) error {
+	tmpl, err := template.ParseFiles("./templates/" + input)
 	if err != nil {
 		return err
 	}
 
-	file, err := os.Create("./configs/" + name)
+	file, err := os.Create("./configs/" + output)
 	if err != nil {
 		return err
 	}
