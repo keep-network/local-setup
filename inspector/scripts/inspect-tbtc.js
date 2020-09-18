2e061cef8795b4181a6b91525bfbb78e57b2cbc6
Dmitry <dymitr.paremski@keep.network>
2020-09-18 17:19:56 +0200

    Removing state transition

--- inspector/scripts/inspect-tbtc.js
+++ inspector/scripts/inspect-tbtc.js
@@ -1,6 +1,5 @@
 const DepositJson = require("@keep-network/tbtc/artifacts/Deposit.json")
 const DepositFactoryJson = require("@keep-network/tbtc/artifacts/DepositFactory.json")
-const BondedECDSAKeepJson = require("@keep-network/keep-ecdsa/artifacts/BondedECDSAKeep.json")
 
 const truffleContract = require("@truffle/contract")
 const contractHelper = require("./lib/contract-helper")
@@ -24,7 +23,7 @@ module.exports = async function() {
             }
         )
 
-        console.log(`Number of created deposits: ${depositCreatedEvents.length}`)
+        console.log(`Number of created deposits: ${depositCreatedEvents.length} \n`)
 
         const depositAddresses = []
         depositCreatedEvents.forEach((event) => 
@@ -61,16 +60,6 @@ module.exports = async function() {
             console.log(`lot size [sat]:  ${lotSizeSatoshis}`)
             console.log(`lot size [tbtc]: ${lotSizeTbtc}`)
 
-            if (stateString == "AWAITING_SIGNER_SETUP") {
-                await retrieveSignerPubkey(deposit)
-            } else if (stateString == "AWAITING_BTC_FUNDING_PROOF") {            
-                await notifyFundingTimedOut(deposit)
-            } else if (stateString == "AWAITING_WITHDRAWAL_SIGNATURE") {
-                await provideRedemptionSignature(deposit, keepAddress)
-            } else if (stateString == "AWAITING_WITHDRAWAL_PROOF") {
-                await notifyRedemptionProofTimeout(deposit)
-            }
-
             console.log(``)
         }
 
@@ -78,94 +67,5 @@ module.exports = async function() {
     } catch (error) {
       console.log(error)
       process.exit()
-    }
-}
-
-async function notifySignerSetupFailed(deposit) {
-    console.log(`Notifying signer setup failed...`)
-    try {
-        const receipt = await deposit.notifySignerSetupFailed()
-        console.log(`TX: ${receipt.tx}`)
-    } catch (error) {
-        console.log(`Failed with: ${JSON.stringify(error)}`)
-    }
-}
-
-async function retrieveSignerPubkey(deposit) {
-    console.log(`Retrieving signer pubkey...`)
-    try {
-        const receipt = await deposit.retrieveSignerPubkey()
-        console.log(`TX: ${receipt.tx}`)
-    } catch (error) {
-        console.log(`Failed with: ${JSON.stringify(error)}`)
-    }
-}
-
-async function notifyFundingTimedOut(deposit) {
-    console.log(`Notifying funding timeout...`)
-    try {
-        const receipt = await deposit.notifyFundingTimedOut()
-        console.log(`TX: ${receipt.tx}`)
-    } catch (error) {
-        console.log(`Failed with: ${JSON.stringify(error)}`)
-    }
-}
-
-async function provideRedemptionSignature(deposit, keepAddress) {
-    const BondedECDSAKeep = truffleContract(BondedECDSAKeepJson)
-    BondedECDSAKeep.setProvider(web3.currentProvider)
-
-    const keep = await BondedECDSAKeep.at(keepAddress)
-
-    console.log(`Looking for the signature from keep ${keepAddress}...`)
-
-    console.log(await keep.digest())
-    const signatureSubmittedEvents = await keep.getPastEvents(
-        "SignatureSubmitted",
-        {
-            fromBlock: factoryDeploymentBlock,
-            toBlock: "latest",
-        }
-    )
-
-    const signaturesCount = signatureSubmittedEvents.length
-    console.log(`Number of signatures submitted: ${signaturesCount}`)
-   
-    if (signaturesCount == 0) {
-        // no signatures, nothing to do here
-        return
-    }
-    const lastSignature = signatureSubmittedEvents[signaturesCount - 1]
-
-    const {digest, r, s, recoveryID } = lastSignature.returnValues
-    // A constant in the Ethereum ECDSA signature scheme, used for public key recovery [1]
-    // Value is inherited from Bitcoin's Electrum wallet [2]
-    // [1] https://bitcoin.stackexchange.com/questions/38351/ecdsa-v-r-s-what-is-v/38909#38909
-    // [2] https://github.com/ethereum/EIPs/issues/155#issuecomment-253810938
-    const ETHEREUM_ECDSA_RECOVERY_V = web3.utils.toBN(27)
-    const v = web3.utils.toBN(recoveryID).add(ETHEREUM_ECDSA_RECOVERY_V)
-
-    console.log(`digest = ${digest}, r = ${r}, s = ${s}, v = ${v}`)
-    
-    try {
-        console.log(`Providing redemption signature...`)
-        const receipt = await deposit.provideRedemptionSignature(
-            v,
-            r.toString(),
-            s.toString()
-        )
-        console.log(`TX: ${receipt.tx}`)
-    } catch (error) {
-        console.log(`Failed with: ${JSON.stringify(error)}`)
-    }
-}
-
-async function notifyRedemptionProofTimeout(deposit) {
-    console.log(`Notifying redemption proof timeout...`)
-    try {
-        const receipt = await deposit.notifyRedemptionProofTimedOut()
-        console.log(`TX: ${receipt.tx}`)
-    } catch (error) {
-        console.log(`Failed with: ${JSON.stringify(error)}`)
     }
 }
