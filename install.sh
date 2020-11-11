d9ea5ebae5017ec74f080efec404098a4031af0d
Dmitry <dymitr.paremski@keep.network>
2020-11-11 09:30:06 +0100

    Testing changes for e2e test

--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@ set -e
 # Install tBTC.
 ./install-tbtc.sh
 
+# Install tBTC dApp.
+./install-tbtc-dapp.sh
+
 # Do not install keep network dApps for e2e nightly test
 if [[ $E2E_TEST != true ]]
 then
     # Install Keep Dashboard.
     ./install-keep-dashboard.sh
-
-    # Install tBTC dApp.
-    ./install-tbtc-dapp.sh
 fi
 
 echo "Installation script executed successfully with versions of submodules:\n$(git submodule)"
