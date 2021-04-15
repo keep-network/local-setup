71d1b0b988454a4c75f48708e6f6aa0d1f25019e
Beau Shinkle <beaushinkle@gmail.com>
2021-04-15 11:39:10 -0400

    Add sendRawTransaction and getBalance
    These additional methods provide convenience around verifying the liquidation recovery flow

--- bitcoind-wallet/index.js
+++ bitcoind-wallet/index.js
@@ -14,3 +14,14 @@ export async function getNewAddress() {
   console.log(address)
   return address
 }
+
+export async function sendRawTransaction(transaction) {
+	return bitcoinRpc.sendrawtransactionAsync(transaction)
+}
+
+export async function getBalance(address) {
+	await bitcoinRpc.importaddressAsync(address)
+	const balance = (await bitcoinRpc.getreceivedbyaddressAsync(address, 0)).result
+	console.log(balance)
+	return balance
+}
