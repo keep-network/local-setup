19d7de7b3d318e93ab4203cdfc8943436913a74b
Beau Shinkle <beaushinkle@gmail.com>
2021-05-10 10:20:07 -0400

    Add --build to the docker-compose up call
    Without the --build flag, the script won't rebuild the underlying dockers if the Dockerfile contents change.
    
    See: https://stackoverflow.com/questions/39988844/docker-compose-up-vs-docker-compose-up-build-vs-docker-compose-build-no-cach/39988980#39988980

--- run-bitcoin.sh
+++ run-bitcoin.sh
@@ -10,4 +10,4 @@ LOG_END='\n\e[0m' # new line + reset color
 printf "${LOG_START}Starting bitcoind, electrs, and electrumX...${LOG_END}"
 printf "${LOG_START}If the build step fails, try increasing the memory allocated to docker.${LOG_END}"
 
-docker-compose -f ./bitcoin/docker-compose.yml up
+docker-compose -f ./bitcoin/docker-compose.yml up --build
