5773cb6baa8e9568ba179e0966f8953b4934429a
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-05-26 15:47:19 +0200

    Updates geth run command to match geth 1.10 api
    API has changed for http and ws arguments in geth 1.10, here we align
    it.

--- run-geth.sh
+++ run-geth.sh
@@ -14,10 +14,10 @@ printf "${LOG_START}Starting geth...${LOG_END}"
 # --unlock unlocks the signer account for proof of authority block signing,
 # otherwise we can't run the network <_<
 geth --port 0 --networkid 1101 --identity "somerandomidentity" \
-    --ws --wsaddr "127.0.0.1" --wsport "8546" --wsorigins "*" \
-    --rpc --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain "*" \
-    --rpcapi "db,ssh,miner,admin,eth,net,web3,personal,debug" \
-    --wsapi "db,ssh,miner,admin,eth,net,web3,personal,debug" \
+    --ws --ws.addr "127.0.0.1" --ws.port "8546" --ws.origins "*" \
+    --http --http.port "8545" --http.addr "127.0.0.1" --http.corsdomain "*" \
+    --http.api "db,ssh,miner,admin,eth,net,web3,personal,debug" \
+    --ws.api "db,ssh,miner,admin,eth,net,web3,personal,debug" \
     --datadir=$GETH_DATA_DIR --syncmode "fast" \
     --miner.etherbase=$GETH_ETHEREUM_ACCOUNT --mine --miner.threads=1 \
     --unlock $GETH_ETHEREUM_ACCOUNT --password <(echo "password") \
