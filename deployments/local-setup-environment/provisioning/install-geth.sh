f64ad14cdf612134e4f7087ebc5dd54025232c41
Michalina Cienciała <michalina.cienciala@keep.network>
2021-05-27 11:06:22 +0200

    Change Geth config to reduce failures during contracts migration

--- deployments/local-setup-environment/provisioning/install-geth.sh
+++ deployments/local-setup-environment/provisioning/install-geth.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Installing go-ethereum..."
-GETH_PACKAGE=geth-alltools-linux-amd64-1.10.2-97d11b01.tar.gz
+GETH_PACKAGE=geth-alltools-linux-amd64-1.10.3-991384a7.tar.gz
 
 curl -O https://gethstore.blob.core.windows.net/builds/$GETH_PACKAGE
 
