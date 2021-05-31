04d1d865c6e031aa802979ac818654c4179f2ff3
Michalina Cienciała <michalina.cienciala@keep.network>
2021-05-31 10:23:28 +0200

    Temporarily add npm linking step to the test script

--- run-e2e-test.sh
+++ run-e2e-test.sh
@@ -6,4 +6,13 @@ cd e2e
 
 npm install
 
+cd ../tbtc.js
+echo "List (1):"
+ls -l node_modules/@keep-network
+echo "Link:"
+npm link @keep-network/keep-core @keep-network/keep-ecdsa @keep-network/tbtc
+echo "List (2):"
+ls -l node_modules/@keep-network
+cd ../e2e
+
 node --experimental-json-modules e2e-test.js
