aac4cc99692605df5d42e3536dfa864f092aa40f
Michalina Cienciała <michalina.cienciala@keep.network>
2021-05-31 14:22:33 +0200

    Don't run `nvm use ...` in the `install-e2e-test.sh` script

--- install-e2e-test.sh
+++ install-e2e-test.sh
@@ -8,13 +8,10 @@ npm install
 
 echo "Linking..."
 cd ../keep-core/solidity
-nvm use 14.3.0
 npm link
 cd ../../keep-ecdsa/solidity
-nvm use 14.3.0
 npm link
 cd ../../tbtc/solidity
-nvm use 14.3.0
 npm link
 
 cd ../../tbtc.js
