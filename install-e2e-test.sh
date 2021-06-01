4e4e01d1d0ca87894a10f63ed8c73e907919ce23
Michalina Cienciała <michalina.cienciala@keep.network>
2021-06-01 19:59:50 +0200

    Install sha3 dependency before running npm link for keep-ecdsa
    Without the added step, the later step with `npm link` in
    `keep-ecdsa/solidity` fails with the following error:
    ```
    npm ERR! Failed at the sha3@1.2.3 install script.
    npm ERR! This is probably not a problem with npm. There is likely
    additional logging output above.
    ```
    Replacing `npm install --save-dev sha3` with `npm rebuild` (tested with
    previous commit) did not help for the problem, hence the original
    working solution has been brought back.

--- install-e2e-test.sh
+++ install-e2e-test.sh
@@ -6,9 +6,15 @@ echo "Running npm install..."
 cd e2e
 npm install
 
-echo "Rebuilding..."
+# Without the below step with installation of the `sha3`, the later step with
+# `npm link` in `keep-ecdsa/solidity` fails with the following error:
+# ```
+# npm ERR! Failed at the sha3@1.2.3 install script.
+# npm ERR! This is probably not a problem with npm. There is likely
+# additional logging output above.
+# ```
 cd ../keep-ecdsa/solidity
-npm rebuild
+npm install --save-dev sha3
 
 echo "Linking..."
 cd ../../keep-core/solidity
