d9bbd7648ec48af52a2b18fd27a5fd9f5d240ac3
Beau Shinkle <beaushinkle@gmail.com>
2021-07-07 16:57:51 -0400

    Automatically populate beneficiary address

--- install-keep-ecdsa.sh
+++ install-keep-ecdsa.sh
@@ -9,6 +9,10 @@ DONE_END='\n\n\e[0m'    # new line + reset
 
 WORKDIR=$PWD
 
+printf "${LOG_START}Installing bitcoind-wallet...${LOG_END}"
+
+npm install -g "$WORKDIR/bitcoind-wallet"
+
 printf "${LOG_START}Starting keep-ecdsa deployment...${LOG_END}"
 
 printf "${LOG_START}Copying config files...${LOG_END}"
@@ -21,14 +25,26 @@ cd keep-ecdsa/configs
 # Fill absolute paths in config files with actual working directory.
 TMP_FILE=$(mktemp /tmp/config.local.1.toml.XXXXXXXXXX)
 sed 's:WORKDIR:'$WORKDIR':' config.local.1.toml > $TMP_FILE
+# Generate a new btc wallet address
+BENEFICIARY_ADDRESS=$(NODE_NO_WARNINGS=1 bitcoind-wallet getNewAddress | sed 's/ *$//g')
+# Set the address at Extensions.TBTC.Bitcoin.BeneficiaryAddress
+sed -i '' "s/BENEFICIARY_ADDRESS/$BENEFICIARY_ADDRESS/g" $TMP_FILE
 mv $TMP_FILE config.local.1.toml
 
 TMP_FILE=$(mktemp /tmp/config.local.2.toml.XXXXXXXXXX)
 sed 's:WORKDIR:'$WORKDIR':' config.local.2.toml > $TMP_FILE
+# Generate a new btc wallet address
+BENEFICIARY_ADDRESS=$(NODE_NO_WARNINGS=1 bitcoind-wallet getNewAddress | sed 's/ *$//g')
+# Set the address at Extensions.TBTC.Bitcoin.BeneficiaryAddress
+sed -i '' "s/BENEFICIARY_ADDRESS/$BENEFICIARY_ADDRESS/g" $TMP_FILE
 mv $TMP_FILE config.local.2.toml
 
 TMP_FILE=$(mktemp /tmp/config.local.3.toml.XXXXXXXXXX)
 sed 's:WORKDIR:'$WORKDIR':' config.local.3.toml > $TMP_FILE
+# Generate a new btc wallet address
+BENEFICIARY_ADDRESS=$(NODE_NO_WARNINGS=1 bitcoind-wallet getNewAddress | sed 's/ *$//g')
+# Set the address at Extensions.TBTC.Bitcoin.BeneficiaryAddress
+sed -i '' "s/BENEFICIARY_ADDRESS/$BENEFICIARY_ADDRESS/g" $TMP_FILE
 mv $TMP_FILE config.local.3.toml
 
 printf "${LOG_START}Creating storage directories...${LOG_END}"
