b435261f690e3973e0ac4822ea3a01f07678cb9a
Michalina Cienciała <michalina.cienciala@keep.network>
2022-02-02 14:42:56 +0100

    Change the checkout commit for cov-pools
    Not intended for merge to `main`.

--- install-repositories.sh
+++ install-repositories.sh
@@ -9,5 +9,5 @@ printf "${LOG_START}Initializing submodules...${LOG_END}"
 
 git submodule update --init --recursive --remote --rebase --force
 cd coverage-pools
-git checkout 1d2133eeb544db6c316f7879a6927d05d114929d
-printf "${LOG_START}Coverage-pools switched to 1d2133eeb544db6c316f7879a6927d05d114929d${LOG_END}"
+git checkout 31d3af2039adc8a556e2ed125b28af4a7a16a371
+printf "${LOG_START}Coverage-pools switched to 31d3af2039adc8a556e2ed125b28af4a7a16a371${LOG_END}"
