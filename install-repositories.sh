0762872c20d8ba157322d1273921d9789d53a47f
Michalina Cienciała <michalina.cienciala@keep.network>
2022-02-02 12:34:42 +0100

    Checkout specific commit during `coverage-pools` install
    This change is not meant to be merged to `main`. It's just a temporary
    config to check behaviour of E2E tests workflow at specific version of
    `coverage-pools` code.

--- install-repositories.sh
+++ install-repositories.sh
@@ -8,3 +8,6 @@ LOG_END='\n\e[0m'      # new line + reset color
 printf "${LOG_START}Initializing submodules...${LOG_END}"
 
 git submodule update --init --recursive --remote --rebase --force
+cd coverage-pools
+git checkout 1d2133eeb544db6c316f7879a6927d05d114929d
+printf "${LOG_START}Coverage-pools switched to 1d2133eeb544db6c316f7879a6927d05d114929d${LOG_END}"
