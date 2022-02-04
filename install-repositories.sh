df54161b271b1caf81fdae7f4029255cd287d016
Dmitry <dymitr.paremski@keep.network>
2022-02-04 14:27:37 +0100

    Testing cov pool gas limit

--- install-repositories.sh
+++ install-repositories.sh
@@ -9,5 +9,5 @@ printf "${LOG_START}Initializing submodules...${LOG_END}"
 
 git submodule update --init --recursive --remote --rebase --force
 cd coverage-pools
-git checkout 31d3af2039adc8a556e2ed125b28af4a7a16a371
+git checkout gas-limit-fix
 printf "${LOG_START}Coverage-pools switched to 31d3af2039adc8a556e2ed125b28af4a7a16a371${LOG_END}"
